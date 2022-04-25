9e04139a163122d13def2e2f0a9780913caa3204
Pulkit Kathuria <kevincobain2000@gmail.com>
2022-04-25 20:31:13 +0900

    clean up scripts

--- server/build.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-export https_proxy=
-export http_proxy=
-
-GOOS=linux GOARCH=amd64 go build main.go
--- server/start_server.sh
+++ server/start_server.sh
@@ -2,6 +2,5 @@
 
 source ~/.bashrc
 
-port=$1
 kill -9 `cat ./pids/*.pid`
-./main $port > /dev/null 2>&1 &
+./main 3006 > /dev/null 2>&1 &
