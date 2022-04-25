db6d7c8c3179c542b5c4cb3bd6cc483b42d395d7
Pulkit Kathuria <kevincobain2000@gmail.com>
2022-04-25 20:29:03 +0900

    clean up envs

--- server/build.sh
+++ server/build.sh
@@ -2,6 +2,5 @@
 
 export https_proxy=
 export http_proxy=
-cp .env.prod .env
 
 GOOS=linux GOARCH=amd64 go build main.go
--- server/start_server.sh
+++ server/start_server.sh
@@ -2,5 +2,6 @@
 
 source ~/.bashrc
 
+port=$1
 kill -9 `cat ./pids/*.pid`
 ./main $port > /dev/null 2>&1 &
